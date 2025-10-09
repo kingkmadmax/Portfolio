d20f49bd92d94ddcdb319f4ed061d7ccb5b2c46b
kirubel <kirubelmamushet@gmail.com>
2025-10-09 12:00:29 -0500

    fex the gred problem and add the filtrashen part

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { CodeBracketIcon,EyeIcon } from "@heroicons/react/24/solid";
 import Link from 'next/link';
-const ProjectCard =({ imgUrl,title,description}) =>{ 
+const ProjectCard =({ imgUrl,title,description,gitUrl,previwUrl }) =>{ 
     return (
         <div>
                
@@ -11,16 +11,17 @@ const ProjectCard =({ imgUrl,title,description}) =>{
              >
                
               <div className="overlay flex items-center justify-center gap-4 absolute top-0 left-0 w-full h-full bg-[#00000088] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-  <Link href="/">
+  <Link href="gitUrl">
     <div className="h-14 w-14 flex items-center justify-center m-5 border border-[#ADB7BE] hover:border-white rounded-full group/link">
       <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover:text-white" />
     </div>
   </Link>
 
-  <Link href="/">
+  <Link href="previwUrl">
     <div className="h-14 w-14 flex items-center justify-center mr-0 border border-[#ADB7BE] hover:border-white rounded-full group/link">
       <EyeIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover:text-white" />
     </div>
+
   </Link>
 </div>
 
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,64 +1,106 @@
-import React from "react"
-
+"use client"
+import React,{useState} from "react"
 import ProjectCard from "./ProjectCard";
- 
+import ProjectTag from "./ProjectTag";
 const Project_Data =[
     {
         id:1,
         title:"react",
         description:"project 1",
         image:"/image/project/4.png",
-        tag:["all","web"]
+        tag:["All","Web"],
+        gitUrl:"/",
+        previwUrl:"/"
     },
      {
         id:2,
         title:"java",
         description:"project 2",
         image:"/image/project/1.jpg",
-        tag:["all","web"]
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
     },
      {
         id:3,
         title:"python",
         description:"project 3",
         image:"/image/project/2.jpg",
-        tag:["all","web"]
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
     },
      {
         id:4,
         title:"Sql",
         description:"project 4",
         image:"/image/project/3.jpg",
-        tag:["all","web"]
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
     },
      {
         id:5,
         title:"ruby",
         description:"project 5",
         image:"/image/project/5.png",
-        tag:["all","web"]
+        tag:["All","App"],
+         gitUrl:"/",
+        previwUrl:"/"
     },
     {
         id:6,
         title:"fluter",
         description:"project 6",
         image:"/image/project/6.jpg",
-        tag:["all","web"]
+        tag:["All","App"],
+         gitUrl:"/",
+        previwUrl:"/"
     },
 ] 
 
 const ProjectSection =() =>{
+    const [tag,setTag] =useState("All");
+    const handleTagChange =(newTag) =>{
+        setTag(newTag);
+
+    };
+    const filteredProjects = Project_Data.filter((project)=>
+        project.tag.includes(tag)
+    );
     return (
         <>
-            <h2 className="text-center text-4xl font-bold text-white mt-4">My projects</h2>
-            <div>{Project_Data.map((project) => 
+            <h2 className="text-center text-4xl font-bold text-white mt-4">My projects
+
+            </h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+              <ProjectTag
+               onClick={handleTagChange}
+               name="All"
+               isSelected = {tag ==='All'}
+               />
+               <ProjectTag
+               onClick={handleTagChange}
+               name="Web"
+               isSelected = {tag ==='Web'}
+               />
+               <ProjectTag
+               onClick={handleTagChange}
+               name="App"
+               isSelected = {tag ==='App'}
+               />
+            </div>
+           <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+            {filteredProjects.map((project) => 
             <ProjectCard 
             key={project.id}
-            
             title={project.title}
             description={project.description}
             imgUrl={project.image}
             tags={project.tag}
+            gitUrl={project.gitUrl}
+            previwUrl={project.previwUrl}
+
             /> )}</div>
         </>
 
