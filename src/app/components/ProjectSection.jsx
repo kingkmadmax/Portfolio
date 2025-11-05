f7f0501689e659b75871cc4b35735c07aebd018a
kirubel <kirubelmamushet@gmail.com>
2025-11-05 06:07:15 -0500

    Revert "finshed ading the animation"
    This reverts commit 48bf773a89700b501f8f456eba30ab026379d4f6.

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,69 +1,125 @@
 "use client"
-import React, { useState, useRef } from "react";
+import React,{useState,useRef} from "react"
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
-import { motion, useInView } from "framer-motion";
+import { animate, motion, useInView } from "framer-motion";
+const Project_Data =[
+    {
+        id:1,
+        title:"react",
+        description:"project 1",
+        image:"/image/project/4.png",
+        tag:["All","Web"],
+        gitUrl:"/",
+        previwUrl:"/"
+    },
+     {
+        id:2,
+        title:"java",
+        description:"project 2",
+        image:"/image/project/1.jpg",
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
+    },
+     {
+        id:3,
+        title:"python",
+        description:"project 3",
+        image:"/image/project/2.jpg",
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
+    },
+     {
+        id:4,
+        title:"Sql",
+        description:"project 4",
+        image:"/image/project/3.jpg",
+        tag:["All","Web"],
+         gitUrl:"/",
+        previwUrl:"/"
+    },
+     {
+        id:5,
+        title:"ruby",
+        description:"project 5",
+        image:"/image/project/5.png",
+        tag:["All","App"],
+         gitUrl:"/",
+        previwUrl:"/"
+    },
+    {
+        id:6,
+        title:"fluter",
+        description:"project 6",
+        image:"/image/project/6.jpg",
+        tag:["All","App"],
+         gitUrl:"/",
+        previwUrl:"/"
+    },
+] 
 
-const Project_Data = [
-  { id: 1, title: "react", description: "project 1", image: "/image/project/4.png", tag: ["All", "Web"], gitUrl: "/", previwUrl: "/" },
-  { id: 2, title: "java", description: "project 2", image: "/image/project/1.jpg", tag: ["All", "Web"], gitUrl: "/", previwUrl: "/" },
-  { id: 3, title: "python", description: "project 3", image: "/image/project/2.jpg", tag: ["All", "Web"], gitUrl: "/", previwUrl: "/" },
-  { id: 4, title: "Sql", description: "project 4", image: "/image/project/3.jpg", tag: ["All", "Web"], gitUrl: "/", previwUrl: "/" },
-  { id: 5, title: "ruby", description: "project 5", image: "/image/project/5.png", tag: ["All", "App"], gitUrl: "/", previwUrl: "/" },
-  { id: 6, title: "fluter", description: "project 6", image: "/image/project/6.jpg", tag: ["All", "App"], gitUrl: "/", previwUrl: "/" },
-];
+const ProjectSection =() =>{
+    const [tag,setTag] =useState("All");
+    const ref = useRef(null);
+    const isInView =useInView(ref,{once:true});
+    const handleTagChange =(newTag) =>{
+        setTag(newTag);
 
-const ProjectSection = () => {
-  const [tag, setTag] = useState("All");
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+    };
+    const filteredProjects = Project_Data.filter((project)=>
+        project.tag.includes(tag)
 
-  const handleTagChange = (newTag) => setTag(newTag);
+    );
 
-  const filteredProjects = Project_Data.filter((project) =>
-    project.tag.includes(tag)
-  );
+    const cardVariants ={
+        initial:{y:50,opacity:0},
+        animate:{y:0,opacity:1},
+    };
+    return (
+        <section ref={ref}>
+            <h2 className="text-center text-4xl font-bold text-white mt-4">My projects
 
-  const cardVariants = {
-    initial: { y: 50, opacity: 0 },
-    animate: { y: 0, opacity: 1 },
-  };
-
-  return (
-    <section>
-      <h2 className="text-center text-4xl font-bold text-white mt-4">
-        My Projects
-      </h2>
-
-      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
-        <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
-        <ProjectTag onClick={handleTagChange} name="App" isSelected={tag === "App"} />
-      </div>
-
-      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project, index) => (
-          <motion.li
-            key={index}
+            </h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+              <ProjectTag
+               onClick={handleTagChange}
+               name="All"
+               isSelected = {tag ==='All'}
+               />
+               <ProjectTag
+               onClick={handleTagChange}
+               name="Web"
+               isSelected = {tag ==='Web'}
+               />
+               <ProjectTag
+               onClick={handleTagChange}
+               name="App"
+               isSelected = {tag ==='App'}
+               />
+            </div>
+           <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+            {filteredProjects.map((project) => 
+            <motion.li
             variants={cardVariants}
             initial="initial"
-            animate={isInView ? "animate" : "initial"}
-            transition={{ duration: 0.3, delay: index * 0.4 }}
-          >
-            <ProjectCard
-              key={project.id}
-              title={project.title}
-              description={project.description}
-              imgUrl={project.image}
-              tags={project.tag}
-              gitUrl={project.gitUrl}
-              previwUrl={project.previwUrl}
-            />
-          </motion.li>
-        ))}
-      </ul>
-    </section>
-  );
-};
+            animate={isInView ?"animate":"inital"}
+            >
+            <ProjectCard 
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            tags={project.tag}
+            gitUrl={project.gitUrl}
+            previwUrl={project.previwUrl}
 
+            />
+            
+            </motion.li> )}</div>
+        </section>
+
+    )
+}
 export default ProjectSection;
--- src/app/components/TabButtons.jsx
+++ src/app/components/TabButtons.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
-
-const variants ={
- default:{width:0},
-active:{width:"calc(100% - 0.75rem)"},
-};
 
 const TabButtons = ({ active, selectTab, children }) => {
   const buttonClasses = active
-    ? "text-white "
+    ? "text-white border-b border-purple-500"
     : "text-[#ADB7BE]";
 
   return (
@@ -16,11 +10,6 @@ const TabButtons = ({ active, selectTab, children }) => {
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
-      <motion.div
-        animate={active ? "active" :"default"}
-        variants={variants}
-        className="h-1 bg-yellow-500 mt-2 mr-3">
-      </motion.div>
     </button>
   );
 };
